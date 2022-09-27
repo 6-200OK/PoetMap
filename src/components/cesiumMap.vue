edd757aeb9418072e96ffe191cbaa65f3b649213
lee <1926557917@qq.com>
2022-09-27 20:57:42 +0800

    error import cesium

---
+++ src/components/cesiumMap.vue
@@ -0,0 +1,43 @@
+<template>
+	<view class="map_content">	
+		<view id="container"></view>
+		</view>
+</template>
+
+<script module="CesiumMap" lang="renderjs">
+window.CESIUM_BASE_URL = "http://localhost:8180/"	
+import * as Cesium from 'cesium';
+
+	export default {
+		name:"cesiumMap",
+		data() {
+			return {
+				mapViewer: null,
+			};
+		},
+		mounted() {
+			this.init_cesium_map()
+		},
+		methods:{
+			init_cesium_map(){
+				Cesium.Ion.defaultAccessToken ='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiIyNDY1NmZiNS0wYmU1LTRkNTgtODA0Yi1jMzI0NGE4ZTA2ZjciLCJpZCI6NTc3ODksImlhdCI6MTYyMjYxNzA4OX0.1sRx-WfaOPxGx-ogkElxlWmbRwUa_8oiT9hQgsIvvjk';
+				this.mapViewer = new Cesium.Viewer("container")
+			}
+		}
+	}
+</script>
+
+<style>
+.map_content {
+		/* display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center; */
+		width: 100vw;
+		height: 100vh;
+	}
+	#container {
+		width: 100%;
+		height: 100%;
+	}
+</style>
---
+++ src/pages/index.vue
@@ -0,0 +1,49 @@
+<template>
+  <view class="content">
+    <CeisumMap></CeisumMap>
+  </view>
+</template>
+
+<script>
+	import CeisumMap from "../components/cesiumMap.vue"
+export default {
+  data() {
+    return {
+      title: 'Hello',
+    }
+  },
+  components:{
+	CeisumMap  
+  }
+}
+</script>
+
+<style>
+.content {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100vw;
+  height: 100vh;
+}
+
+.logo {
+  height: 200rpx;
+  width: 200rpx;
+  margin-top: 200rpx;
+  margin-left: auto;
+  margin-right: auto;
+  margin-bottom: 50rpx;
+}
+
+.text-area {
+  display: flex;
+  justify-content: center;
+}
+
+.title {
+  font-size: 36rpx;
+  color: #8f8f94;
+}
+</style>
