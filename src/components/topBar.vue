81f27fd0b6471a5ea99a8aa29aa0111190fc56de
Noah-sychronized <1264918006@qq.com>
2022-10-07 09:53:08 +0800

    banner栏修改

--- src/components/topBar.vue
+++ src/components/topBar.vue
@@ -10,8 +10,8 @@ const currentPointInfo = computed(()=>{
 <template>
 <div class="top">
 	<div class="bar">
-		<p class="title">追寻诗人的足迹</p>
-		<img src="../static/topbar.png" alt="">
+		<p class="title">寻踪·诗意盛唐</p>
+		<img src="../static/banner.png" alt="">
 	</div>
 	<div class="currentInfo">
 		<h1 class="currentAdressName">{{currentPointInfo?.name}}</h1>
@@ -21,22 +21,21 @@ const currentPointInfo = computed(()=>{
 </template>
 <style scoped>
 .bar{
-	background-color: #08317D;
 	width: 100vw;
 	height: 4vh;
 }
 .bar img{
-	width: 40vw;
-	margin-left: 30vw;
-	height: 6vh;
+	width: 26vw;
+	margin-left: 38vw;
+	height: 17vh;
+	margin-top: -1.5vh;
 }
 .bar .title{
 	position: absolute;
-	color: white;
 	left: 44vw;
-	top: 1vh;
-	font-size: 70rpx;
-	font-family: 'Times New Roman', Times, serif;
+	top: 4vh;
+	font-size: 90rpx;
+	font-family: 'FZJT', Times, serif;
 }
 .currentInfo{
 	width: 10vw;
--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import App from "./App.vue";
 import { setupStore } from "./stores";
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
+import "@/static/text/text.css"
 export function createApp() {
 	const app = createSSRApp(App);
 	setupStore(app),
