0f215128fe414e6bf96c114d4f030f1f1b004b00
lyuly <liumike567@gmail.com>
2022-10-07 17:30:06 +0800

    添加popper显示诗人简介

--- src/components/poetLife.vue
+++ src/components/poetLife.vue
@@ -1,7 +1,8 @@
 <script setup>
 import { userStore } from '../stores/modules/user';
 import { computed } from 'vue';
-import { flyToPoint, addModel } from '../utils/mapUtils.js'
+import { ElPopover, ElButton } from 'element-plus';
+import { Pointer } from '@element-plus/icons-vue'
 
 const store = userStore();
 
@@ -9,35 +10,24 @@ const store = userStore();
 const poetLife = computed(() => {
   return store.introduction;
 })
-// 获取诗人足迹对应的经纬度
-const goToPoint = (lon, lat,name) => {
-	store.setCurrentAdressName({
-    lon,lat,name
-  })
-  // 根据经纬度进行定位
-  flyToPoint(lon, lat);
-  addModel(lon, lat);
-}
 
 </script>
 
 <template>
   <div v-show="poetLife">
-    <el-card>
-      <div class="poetLife" :title="poetLife" :color="red">
-        {{poetLife}}
-      </div>
-    </el-card>
+    <el-popover
+      placement="right"
+      title="简介"
+      :width="200"
+      trigger="hover"
+      :content="poetLife"
+      >
+      <template #reference>
+        <el-button type="success" circle :icon="Pointer"></el-button>
+      </template>
+    </el-popover>
   </div>
 </template>
-<!-- 设计诗人生平介绍样式 -->
+
 <style scoped lang="sass">
-  .poetLife
-    font-size: 1em
-    display: -webkit-box
-    text-overflow: ellipsis
-    word-break: break-all
-    -webkit-line-clamp: 10
-    -webkit-box-orient: vertical
-    overflow: hidden
 </style>
